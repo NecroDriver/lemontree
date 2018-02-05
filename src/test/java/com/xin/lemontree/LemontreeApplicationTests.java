46391b4e294b54157acfdd4277709803277c373d
mafh <1193482868@qq.com>
2018-02-05 18:52:01 +0800

    更新部分代码

--- src/test/java/com/xin/lemontree/LemontreeApplicationTests.java
+++ src/test/java/com/xin/lemontree/LemontreeApplicationTests.java
@@ -1,30 +1,27 @@
 package com.xin.lemontree;
 
-import com.xin.lemontree.entity.UserLoginEntity;
-import com.xin.lemontree.tools.Convert.ConvertUtils;
-import com.xin.lemontree.vo.UserLoginVo;
-
-import java.util.Date;
-import java.util.UUID;
+import org.springframework.util.DigestUtils;
 
 public class LemontreeApplicationTests {
 
 	public static void main(String[] args) {
-        UserLoginEntity userLoginEntity = new UserLoginEntity();
-        userLoginEntity.setId(1);
-        userLoginEntity.setAccount("nextTime");
-        userLoginEntity.setUserName("时光如墨");
-        userLoginEntity.setPassword("111111");
-        userLoginEntity.setSalt(UUID.randomUUID().toString());
-        userLoginEntity.setEncryptPassword("2222222");
-        userLoginEntity.setCreateTime(new Date());
-        userLoginEntity.setCreatorIP("localhost");
-        userLoginEntity.setModifyTime(new Date());
-        System.out.println(userLoginEntity);
-//        UserLoginVo userLoginVo = new UserLoginVo();
-//        BeanUtils.copyProperties(userLoginEntity, userLoginVo);
-        UserLoginVo userLoginVo = ConvertUtils.convert(userLoginEntity, UserLoginVo.class);
-        System.out.println(userLoginVo);
+//        UserLoginEntity userLoginEntity = new UserLoginEntity();
+//        userLoginEntity.setId(1);
+//        userLoginEntity.setAccount("nextTime");
+//        userLoginEntity.setUserName("时光如墨");
+//        userLoginEntity.setPassword("111111");
+//        userLoginEntity.setSalt(UUID.randomUUID().toString());
+//        userLoginEntity.setEncryptPassword("2222222");
+//        userLoginEntity.setCreateTime(new Date());
+//        userLoginEntity.setCreatorIP("localhost");
+//        userLoginEntity.setModifyTime(new Date());
+//        System.out.println(userLoginEntity);
+////        UserLoginVo userLoginVo = new UserLoginVo();
+////        BeanUtils.copyProperties(userLoginEntity, userLoginVo);
+//        UserLoginVo userLoginVo = ConvertUtils.convert(userLoginEntity, UserLoginVo.class);
+//        System.out.println(userLoginVo);
+        System.out.println(DigestUtils.md5DigestAsHex(("111").getBytes()));
+        System.out.println(DigestUtils.md5Digest(("111").getBytes()));
 	}
 
 	public static void sayHello(){
