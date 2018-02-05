bc80cf27749c9c62e16252c62ae4f8b3d47103e6
NecroDriver <necrodriver@foxmail.com>
2018-02-05 09:28:11 +0800

    添加action

--- src/main/java/com/xin/lemontree/controller/user/action/UserAction.java
+++ src/main/java/com/xin/lemontree/controller/user/action/UserAction.java
@@ -1,7 +1,11 @@
 package com.xin.lemontree.controller.user.action;
 
+import com.xin.lemontree.controller.user.service.UserLoginService;
 import com.xin.lemontree.entity.UserLoginEntity;
 import com.xin.lemontree.vo.ResultVo;
+import com.xin.lemontree.vo.UserLoginVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +20,51 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserAction {
 
+    /**
+     * 用户登录service
+     */
+    @Autowired
+    private UserLoginService userLoginService;
+
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResultVo register(UserLoginEntity userLoginEntity){
+    public ResultVo register(UserLoginEntity userLoginEntity) {
         return new ResultVo(true, userLoginEntity);
+    }
+
+    /**
+     * 登录
+     *
+     * @param account
+     * @param password
+     * @return
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResultVo login(String account, String password) {
+        UserLoginVo userLoginVo = userLoginService.login(account, password);
+        return ResultVo.newResultVo(true, "登录成功！", userLoginVo);
+    }
+
+    /**
+     * 登出
+     *
+     * @param token
+     * @return
+     */
+    @RequestMapping(value = "/loginout/{token}", method = RequestMethod.POST)
+    public ResultVo loginout(@PathVariable("token") String token) {
+        userLoginService.loginout(token);
+        return ResultVo.newResultVo(true, "退出成功！", null);
+    }
+
+    /**
+     * 获取用户
+     *
+     * @param token
+     * @return
+     */
+    @RequestMapping(value = "/user/{token}", method = RequestMethod.POST)
+    public ResultVo getUser(String token) {
+        UserLoginVo userLoginVo = userLoginService.queryUserByToken(token);
+        return ResultVo.newResultVo(true, "成功获取用户信息！", userLoginVo);
     }
 }
--- src/main/java/com/xin/lemontree/vo/ResultVo.java
+++ src/main/java/com/xin/lemontree/vo/ResultVo.java
@@ -10,11 +10,17 @@ import lombok.Data;
  */
 @Data
 public class ResultVo<T> {
-    /** 数据结果 */
+    /**
+     * 数据结果
+     */
     private boolean result;
-    /** 数据 */
+    /**
+     * 数据
+     */
     private T data;
-    /** 结果信息 */
+    /**
+     * 结果信息
+     */
     private String message;
 
     public ResultVo(boolean result, String message) {
@@ -25,5 +31,23 @@ public class ResultVo<T> {
     public ResultVo(boolean result, T data) {
         this.result = result;
         this.data = data;
+    }
+
+    public ResultVo(boolean result, String message, T data) {
+        this.result = result;
+        this.message = message;
+        this.data = data;
+    }
+
+    /**
+     * 生成结果静态调用方法
+     *
+     * @param result  结论
+     * @param message 信息
+     * @param data    数据
+     * @return 对象
+     */
+    public static ResultVo newResultVo(boolean result, String message, Object data) {
+        return new ResultVo(result, message, data);
     }
 }
