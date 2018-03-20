72ab02dd6188c3e6cd0da9e598f17ed2513d5605
NecroDriver <necrodriver@foxmail.com>
2018-03-20 15:44:36 +0800

    页面修改

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,5 +1,15 @@
-function getUrlValue (name) {
-    var reg = new RegExp("(^|&)"+name+"=([^&]*)(&|$)");
+function getUrlValue(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
     var result = location.search.substring(1).match(reg);
     return result == null ? null : decodeURIComponent(result[2]);
+}
+
+function getCookieValue(name) {
+    var reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    var result = document.cookie.match(reg);
+    return result == null ? null : result[2];
+}
+
+function setCookie(name, value, maxAge, path, domain, secure) {
+    document.cookie = name + "=" + value + ((maxAge) ? "; max-age=" + maxAge : "") + ((path) ? "; path=" + path : "") + ((domain) ? ";domain=" + domain : "") + ((secure) ? ";secure=" + secure : "");
 }
